096c2d4d8b3d3aca9680bcc92a5e296fcfaaf456
Peace Gusenga <37972802+PeaceGusenga@users.noreply.github.com>
2023-04-30 09:00:35 +0200

    basic Typescript
    will develop into a project later

---
+++ basics.ts
@@ -0,0 +1,28 @@
+export {}
+
+let name = 'Peace';
+console.log('hello ${name}')
+
+
+//Variable types inferred while typeing and throws error if different
+//file type used on same variable
+let a = 10;
+a = 20;
+
+//Variable types
+//number
+let x: number = 2;
+//String
+let language: string = "TypeScript";
+//Boolean
+let answer: boolean = true;
+//undefined
+let u: undefined = undefined;
+//null
+let n:  null = null;
+// also null
+let y;
+
+//Array
+let list1: number[] = [1,2,3]
+
